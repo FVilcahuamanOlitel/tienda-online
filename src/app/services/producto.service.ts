91c8d1b44cb90c7d3393fdd6f3cbe1b52419dd8a
Frans <fvilcahuaman@olitel.pe>
2025-02-04 16:30:25 -0500

    feat: eliminar, get by id and edit

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -5,15 +5,41 @@ import { Producto } from '../models/producto';
   providedIn: 'root'
 })
 export class ProductoService {
-  productos: Producto[] = [
-    new Producto('Pantalón', 130.0),
-    new Producto('Camisa', 80.0),
-    new Producto('Playera', 50.0),
-  ];
+  private idSiguiente = 1;
+  productos: Producto[] = [];
 
-  constructor() { }
+  constructor() {
+    this.inicializarProducto();
+  }
 
-  agregarProducto(producto: Producto) {
-    this.productos.push(producto);
+  private inicializarProducto() {
+    const producto1 = new Producto(this.idSiguiente++, 'Pantalón', 130.0);
+    const producto2 = new Producto(this.idSiguiente++, 'Camisa', 80.0);
+    const producto3 = new Producto(this.idSiguiente++, 'Playera', 50.0);
+
+    this.productos.push(producto1, producto2, producto3);
+  }
+
+  guardarProducto(producto: Producto) {
+    if (producto.id === null) {
+      producto.id = this.idSiguiente++;
+      this.productos.push(producto);
+    } else {
+      const indice = this.productos.findIndex(p => p.id === producto.id);
+      if (indice !== -1) {
+        this.productos[indice] = producto;
+      }
+    }
+  }
+
+  getProductoById(id: number): Producto | undefined {
+    return this.productos.find(producto => producto.id === id);
+  }
+
+  eliminarProducto(id: number) {
+    const indice = this.productos.findIndex(producto => producto.id === id);
+    if (indice !== -1) {
+      this.productos.splice(indice, 1);
+    }
   }
 }
