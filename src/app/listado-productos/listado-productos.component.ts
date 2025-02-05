8c7a235571d2ef7f6979ddc33fca68738ff79195
Frans <fvilcahuaman@olitel.pe>
2025-02-05 15:27:02 -0500

    feat:  boton agregar producto

--- src/app/listado-productos/listado-productos.component.ts
+++ src/app/listado-productos/listado-productos.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { Producto } from '../models/producto';
 import { FormsModule } from '@angular/forms';
 import { ProductoComponent } from "../producto/producto.component";
-import { FormularioComponent } from "../formulario/formulario.component"; 
 import { ProductoService } from '../services/producto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listado-productos',
-  imports: [FormsModule, ProductoComponent, FormularioComponent],
+  imports: [FormsModule, ProductoComponent],
   templateUrl: './listado-productos.component.html'
 })
 export class ListadoProductosComponent {
   productos: Producto[] = [];
 
-  constructor(private productoService: ProductoService) {}
+  constructor(
+    private productoService: ProductoService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.productos = this.productoService.productos;
+  }
+
+  agregarProducto() {
+    this.router.navigate(['agregar']);
   }
 }
